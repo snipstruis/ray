c4883e2bcf4ff0532b0ab749322fe10932b1092a
Douwe van Gijn <kipstad@hotmail.com>
2016-11-17 16:36:42 +0100

    initial commit

---
+++ main.cc
@@ -0,0 +1,35 @@
+#define GL_GLEXT_PROTOTYPES
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
+struct Rgb{ float r,g,b; };
+
+Rgb screenbuffer[1920*1080];
+
+int main(){
+    SDL_Window *win = SDL_CreateWindow("Roaytroayzah", 0, 0, 640, 480, 
+                                       SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
+    SDL_GL_CreateContext(win);
+    glClearColor(0,0,0,1);
+    
+    while(true){
+        // handle events
+        for(SDL_Event e;SDL_PollEvent(&e);){ if(e.type==SDL_QUIT) return 0; }
+
+        // clear screen
+        glClear(GL_COLOR_BUFFER_BIT);
+        int w,h; SDL_GL_GetDrawableSize(win,&w,&h);
+        glViewport(0, 0, w, h);
+
+        // draw pixels
+        for(int y=0; y<h; y++){
+            for(int x=0; x<w; x++){
+                screenbuffer[y*w+x] = (Rgb){(float)y/h,(float)x/w,0.f};
+            }
+        }
+
+        // blit to screen
+        glDrawPixels(w,h,GL_RGB,GL_FLOAT,&screenbuffer);
+        SDL_GL_SwapWindow(win);
+    }
+}
