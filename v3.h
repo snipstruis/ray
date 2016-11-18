f83e8db7120265aa3ecb87703fdabeec6d5fe820
Douwe van Gijn <kipstad@hotmail.com>
2016-11-18 19:14:03 +0100

    first few tests

--- v3.h
+++ v3.h
@@ -8,14 +8,14 @@
 struct v3{
     fl x,y,z;
     v3() = default;
-    IC v3(fl f):x(f),y(f),z(f){};
-    IC v3(fl x, fl y, fl z):x(x),y(y),z(z){};
-    IC void operator+=(v3 v){x+=v.x; y+=v.y; z+=v.z;};
-    IC void operator-=(v3 v){x-=v.x; y-=v.y; z-=v.z;};
-    IC void operator+=(fl f){x+=f;   y+=f;   z+=f;};
-    IC void operator-=(fl f){x-=f;   y-=f;   z-=f;};
-    IC void operator*=(fl f){x*=f;   y*=f;   z*=f;};
-    IC void operator/=(fl f){x/=f;   y/=f;   z/=f;};
+    IC v3(fl f):x(f),y(f),z(f){}
+    IC v3(fl x, fl y, fl z):x(x),y(y),z(z){}
+    IC void operator+=(v3 v){x+=v.x; y+=v.y; z+=v.z;}
+    IC void operator-=(v3 v){x-=v.x; y-=v.y; z-=v.z;}
+    IC void operator+=(fl f){x+=f;   y+=f;   z+=f;}
+    IC void operator-=(fl f){x-=f;   y-=f;   z-=f;}
+    IC void operator*=(fl f){x*=f;   y*=f;   z*=f;}
+    IC void operator/=(fl f){x/=f;   y/=f;   z/=f;}
 };
 
 IC v3 operator+(v3 a, v3 b){return v3(a.x+b.x, a.y+b.y, a.z+b.z);}
@@ -28,9 +28,19 @@ IC v3 cross    (v3 a, v3 b){return v3(a.y*b.z - a.z*b.y,
                                       a.z*b.x - a.x*b.z,
                                       a.x*b.y - a.y*b.x);}
 IC fl lengthsq (v3 v)      {return dot(v,v);}
-I  fl length   (v3 v)      {return sqrtf(lengthsq(v));}//sqrt: not constextpr! 
+I  fl length   (v3 v)      {return sqrtf(lengthsq(v));}//sqrt: not constextpr:(
 I  fl distance (v3 a, v3 b){return length(b-a);}
 I  v3 normalize(v3 v)      {return v/length(v);}
+
+#ifndef NDEBUG
+bool operator==(v3 a, v3 b){return (a.x==b.x) && (a.y==b.y) && (a.z==b.z);}
+
+#include<ostream>
+std::ostream& operator<<(std::ostream& os, const v3& v){
+    os<<"("<<v.x<<", "<<v.y<<", "<<v.z<<")";
+    return os;
+}
+#endif
 
 #undef IC
 #undef I
