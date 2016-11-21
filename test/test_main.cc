bc440730ebcb6313d4e5f55b3576e0b54b65f9fb
Nick Begg <nickbegg@gmail.com>
2016-11-21 20:53:55 +0100

    add test_main

---
+++ test/test_main.cc
@@ -0,0 +1,5 @@
+// see http://www.boost.org/doc/libs/1_59_0/libs/test/doc/html/boost_test/adv_scenarios/entry_point_overview.html
+// the below generates the main() function for the tests. note the #define should come before the include
+
+#define BOOST_TEST_MODULE ray_test_suite
+#include <boost/test/unit_test.hpp>
