c5214b368fd7d0497b87301912f2af432a55e28a
Douwe van Gijn <kipstad@hotmail.com>
2016-11-22 22:33:21 +0100

    plane-intersect v1 untested

--- primitive.h
+++ primitive.h
@@ -1,22 +1,35 @@
 #pragma once
+#include <math.h>
+#include <glm/glm.hpp>
+
+struct Intersection{
+    float distance; // infinite == no hit
+    glm::vec3    normal;
+};
 
 struct Primitive{
     glm::vec3 pos;
     Material mat;
-    virtual float intersects(Ray) = 0;
+    virtual Intersection intersects(Ray) = 0;
 };
 
 struct Plane:Primitive{
     glm::vec3 normal;
-    virtual float intersect(Ray r){
-        return 9999999999;
+    virtual Intersection intersect(Ray ray){
+        assert(glm::length(ray.direction)==0.f);
+        float denom = glm::dot(normal,ray.direction);
+        if(denom> 1e-6f){
+            float dist = glm::dot(ray.origin,normal)/denom;
+            if(dist>=0.f) return {dist,normal};
+        }
+        return {INFINITY,normal};
     };
 };
 
 struct Sphere:Primitive{
     float radius;
-    virtual float intersect(Ray r){
-        return 9999999999;
+    virtual Intersection intersect(Ray r){
+        return {INFINITY,glm::vec3(0,0,1)};
     };
 };
 
