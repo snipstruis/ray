4ac8366f22daab2a0a154a18596a41c859627567
Nick Begg <nickbegg@gmail.com>
2016-11-24 00:09:28 +0100

    *ahem* get static refs compiling

--- primitive.h
+++ primitive.h
@@ -5,13 +5,13 @@
 
 struct Primitive{
     glm::vec3 pos;
-    virtual float distance(const& Ray) = 0;
-    virtual glm::vec3 normal(const& glm::vec3 position) = 0;
+    virtual float distance(const Ray&) = 0;
+    virtual glm::vec3 normal(const glm::vec3& position) = 0;
 };
 
 struct Plane:Primitive{
     glm::vec3 norm;
-    virtual float distance(const& Ray ray){
+    virtual float distance(const Ray& ray){
         assert(glm::length(ray.direction)<(1+1e-6f));
         assert(glm::length(ray.direction)>(1-1e-6f));
         float denom = glm::dot(-norm,ray.direction);
@@ -21,13 +21,13 @@ struct Plane:Primitive{
         }
         return INFINITY;
     };
-    virtual glm::vec3 normal(const& glm::vec3 position){return norm;}
+    virtual glm::vec3 normal(const glm::vec3& position){return norm;}
 };
 
 struct OutSphere:Primitive{
     float radius;
     // stolen from Jacco's slide
-    virtual float distance(const& Ray ray){
+    virtual float distance(const Ray& ray){
         glm::vec3 c = pos - ray.origin;
         float t = glm::dot( c, ray.direction);
         glm::vec3 q = c - t * ray.direction;
@@ -37,6 +37,6 @@ struct OutSphere:Primitive{
         t -= sqrt( r2 - p2 );
         return t>0?t:INFINITY; // no hit if behind ray start
     };
-    virtual glm::vec3 normal(const& glm::vec3 position){return glm::normalize(position-pos);}
+    virtual glm::vec3 normal(const glm::vec3& position){return glm::normalize(position-pos);}
 };
 
