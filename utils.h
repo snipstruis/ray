dc3bd3b7adb6a8a9f91b8ba8eeb41a1bcea0e66b
Nick Begg <nickbegg@gmail.com>
2016-11-24 16:20:37 +0100

    shiney new official EPSiLON

--- utils.h
+++ utils.h
@@ -2,7 +2,7 @@
 
 #include "glm/gtc/constants.hpp"
 
-const float EPSILON = glm::epsilon<float>();
+const float EPSILON = 1e-6f;
 
 bool feq(float a, float b){
     return a < b + EPSILON || a < b - EPSILON;
