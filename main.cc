2fd3b1d7dae7d05010e2bdeb62405d902bf4d204
Nick Begg <nickbegg@gmail.com>
2016-12-04 22:46:13 +0100

    cleanups

--- main.cc
+++ main.cc
@@ -88,7 +88,7 @@ int main(int argc, char* argv[]){
     // setup scene first, so we can bail on error without flashing a window briefly (errors are stdout 
     // for now - maybe should be a dialog box in future).
     Scene s;
-    if(!setupScene(s, "scene/spots2.scene")) {
+    if(!setupScene(s, "scene/point.scene")) {
         std::cout << "failed to setup scene, bailing" << std::endl;
         return -1;
     }
