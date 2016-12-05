e094422fc5e1460a1c3be0efce78ed8f832a3f2e
Nick Begg <nickbegg@gmail.com>
2016-12-05 12:17:03 +0100

    add loading from the commandline

--- main.cc
+++ main.cc
@@ -85,10 +85,15 @@ bool handleEvents(Scene& s, Mode *vis)
 }
 
 int main(int argc, char* argv[]){
+    if(argc != 2) {
+        std::cerr << "usage " << argv[0] << " <scenefile>" << std::endl;
+        return -1;
+    }
+
     // setup scene first, so we can bail on error without flashing a window briefly (errors are stdout 
     // for now - maybe should be a dialog box in future).
     Scene s;
-    if(!setupScene(s, "scene/point.scene")) {
+    if(!setupScene(s, argv[1])) {
         std::cout << "failed to setup scene, bailing" << std::endl;
         return -1;
     }
