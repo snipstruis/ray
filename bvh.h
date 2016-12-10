4bbf61a6d8fb5ea411627712a4d68e020083ef42
Nick Begg <nickbegg@gmail.com>
2016-12-10 14:56:18 +0100

    create new file

---
+++ bvh.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "glm/vec3.hpp"
+
+struct AABB {
+    glm::vec3 a,b;
+};
+
+
+struct BVHNode{
+    AABB bounds;
+    int leftFirst;
+    int count;
+};
+
+// check sizes are as expected 
+static_assert(sizeof(BVHNode) == 32, "BVHNode size");
+
