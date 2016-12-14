5e1df8558439b28047558e19c4d523a2c5debe01
Nick Begg <nickbegg@gmail.com>
2016-12-14 18:21:47 +0100

    ongoing

--- bvh_build.h
+++ bvh_build.h
@@ -3,22 +3,6 @@
 #include "bvh.h"
 
 #if 0
-void subdivide(TriangleSet const& triangles, BVH& bvh, BVHNode& node, std::uint32_t start, std::uint32_t count, int axis) {
-
-    if(count <= 3) {
-        // std::cout << "subdivide leaf start " << start << " count " << count << " axis " << axis <<std::endl;;
-        // ok, leafy time.
-        node.leftFirst = start;
-        // give this node a count, which by definition makes it a leaf
-        node.count = count;
-        assert(node.isLeaf());
-        assert(node.leftFirst == node.first());
-
-        calcAABBIndirect(node.bounds, triangles, bvh.indicies, node.first(), node.count);
-        std::cout << "leaf AABB " << node.bounds << " count " << node.count << std::endl;
-    }
-    else {
-//        std::cout << "subdivide mid start " << start << " count " << count << " axis " << axis <<std::endl;;
         float sum = 0.0f;
         // non-leaf node.
         for (std::uint32_t i = start; i < (start + count) ; i++) {
@@ -34,57 +18,8 @@ void subdivide(TriangleSet const& triangles, BVH& bvh, BVHNode& node, std::uint3
 
         // get average. div by 3*triangles (ie once for each vertex)
         float average = sum / (count * 3.0f);
-
-//        std::cout << "av " << average << " axis " << axis << " count " << count << std::endl;
-
-        int i = start;
-        int j = start + count - 1;
-
-        while (i < j) {
-            assert(bvh.indicies[i] < triangles.size());
-            assert(bvh.indicies[j] < triangles.size());
-
-            if(triangles[bvh.indicies[i]].getCentroid()[axis] < average) {
-                i++;
-                continue;
-            }
-            if(triangles[bvh.indicies[j]].getCentroid()[axis] > average) {
-                j--;
-                continue;
-            }
-
-#if 0
-            std::cout << "swap " << i << " " << j << std::endl;
-            std::cout << "ii " << triangles[bvh.indicies[i]].getCentroid()[axis] << " ";
-            std::cout << "jj " << triangles[bvh.indicies[j]].getCentroid()[axis] << " ";
-            std::cout << std::endl;
-#endif
-
-            int temp = bvh.indicies[j];
-            bvh.indicies[j] = bvh.indicies[i];
-            bvh.indicies[i] = temp;
-
-            i++;
-            j--;
-        }
-
-        // alloc child nodes
-        node.leftFirst = bvh.nextFree;
-        BVHNode& left = bvh.allocNextNode();
-        BVHNode& right = bvh.allocNextNode();
-        
-        std::uint32_t halfCount = count / 2;
-
-        // beware odd numbers..!
-        std::uint32_t secondRange = halfCount + (count % 2);
         
         int nextAxis = (axis + 1) % 3;
-        // recurse
-        subdivide(triangles, bvh, left, start, halfCount, nextAxis);
-        subdivide(triangles, bvh, right, (start + halfCount), secondRange, nextAxis);
-
-        // now subdivide's done, combine aabb 
-        combineAABB(node.bounds, left.bounds, right.bounds);
     }
 }
 #endif
@@ -164,19 +99,12 @@ inline BVH* buildBVH(Scene& s) {
     BVH* bvh = new BVH;
 
     // setup index map
+    bvh->indicies.resize(s.primitives.triangles.size());
     for (unsigned int i = 0; i < s.primitives.triangles.size(); i++)
         bvh->indicies[i] = i;
 
-    // setup root node
-//    bvh->root().leftFirst = 0;
-//    bvh->root().count = s.primitives.triangles.size();
-//    bvh->indicies.resize(s.primitives.triangles.size());
-
-
-
-//    calcAABBIndirect(bvh->root().bounds, s.primitives.triangles, bvh->indicies, 0, bvh->root().count);
-
- //   std::cout << "stupid AABB " << bvh->root().bounds << std::endl;
+    // recurse and subdivide
+    subdivide<Splitter>(s.primitives.triangles, *bvh, bvh->root(), 0, s.primitives.triangles.size(), 2);
     return bvh;
 }
 
@@ -184,25 +112,27 @@ inline BVH* buildBVH(Scene& s) {
 // so traversing it will degrade to a linear search. 
 // Useful for testing worst case scenarios, or feeling bad about yourself.
 struct StupidSplitter {
-    bool GetSplit(
-            int& axis,                      // out: axis on which to split
+    static bool GetSplit(
+            unsigned int& axis,             // out: axis on which to split
             float& splitPoint,              // out: point on this axis at which to split
             TriangleSet const& triangles,   // in: master triangle array
             BVH& bvh,                       // in: BVH root 
             BVHNode& node,                  // in: current bvh node (to split)
             std::uint32_t start,            // in: start index in triangle array
             std::uint32_t count,            // in: num triangles (ie start+count <= triangles.size())
-            int lastAxis) {                 // in: the axis on which the parent was split
+            unsigned int lastAxis) {        // in: the axis on which the parent was split
 
-        return true; // stop splitting
+        return false; // stop splitting
     }
 };
 
 inline BVH* buildStupidBVH(Scene& s) {
     BVH* bvh = buildBVH<StupidSplitter>(s);
+
     // as we have a single node, it should be a leaf
     assert(bvh->nodeCount() == 1);
     assert(bvh->root().isLeaf());
+    std::cout << "stupid AABB " << bvh->root().bounds << std::endl;
     return bvh;
 }
 
