3c994a7d041c76ad4b62cbbaa39ca3c64c8030a8
Nick Begg <nickbegg@gmail.com>
2016-12-15 20:04:30 +0100

    create simple timer type

--- utils.h
+++ utils.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "glm/gtc/constants.hpp"
+
 #include <algorithm>
+#include <chrono>
 #include <cmath>
 
 const float EPSILON = 1e-6f;
@@ -10,6 +12,22 @@ const float PI = glm::pi<float>();
 inline bool feq(float a, float b){
     return a < b + EPSILON || a < b - EPSILON;
 }
+
+struct Timer {
+    Timer() : last(std::chrono::high_resolution_clock::now()) {}
+
+    // returns seconds since last sample
+    float sample() {
+        auto now = std::chrono::high_resolution_clock::now();
+        // for some reason, std::seconds doesn't seem to be defined on my sys
+        float diff = 
+            std::chrono::duration_cast<std::chrono::duration<float,std::milli>>(now - last).count() / 1000.0f;
+        last = now;
+        return diff;
+    }
+    
+    std::chrono::time_point<std::chrono::high_resolution_clock> last;
+};
 
 // stupid lack of c++17
 template<class T>
