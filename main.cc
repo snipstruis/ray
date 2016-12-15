023b663417096b5fcd466fdca7841d686026ef0c
Nick Begg <nickbegg@gmail.com>
2016-12-15 20:56:05 +0100

    more commandline

--- main.cc
+++ main.cc
@@ -11,16 +11,16 @@
 int width = 640;
 int height = 640;
 
-enum  optionIndex { UNKNOWN, HELP, PLUS };
+enum  optionIndex {UNKNOWN, HELP, INPUT_DIR, OUTPUT_DIR, SCENE, MODE};
 const option::Descriptor usage[] =
 {
-	{UNKNOWN, 0,"" , ""    ,option::Arg::None, "USAGE: example [options]\n\n"
-                                               "Options:" },
-	{HELP,    0,"" , "help",option::Arg::None, "  --help  \tPrint usage and exit." },
-	{PLUS,    0,"p", "plus",option::Arg::None, "  --plus, -p  \tIncrement count." },
-	{UNKNOWN, 0,"" ,  ""   ,option::Arg::None, "\nExamples:\n"
-                                               "  example --unknown -- --this_is_no_option\n"
-                                               "  example -unk --plus -ppp file1 file2\n" },
+	{UNKNOWN,    0,"" , "",           option::Arg::None, "USAGE: ray [options]\n\nOptions:"},
+	{HELP,       0,"" , "help",       option::Arg::None, "  --help  \tPrint usage and exit."},
+	{INPUT_DIR,  0,"i", "input-dir",  option::Arg::None, "  --input-dir, -i  \tinput directory"},
+	{SCENE,      0,"s", "scene",      option::Arg::None, "  --scene, -s  \tscene file"},
+	{OUTPUT_DIR, 0,"o", "output-dir", option::Arg::None, "  --output-dir, -o  \toutput directory"},
+	{MODE,       0,"m", "mode",       option::Arg::None, "  --mode, -m  \trender mode [batch, interactive]"},
+
 	{0,0,0,0,0,0}
 };
 
@@ -40,8 +40,10 @@ int main(int argc, char* argv[]){
      	return 0;
    	}
 
+#if 0
    	std::cout << "--plus count: " <<
      	options[PLUS].count() << "\n";
+#endif
 
    	for (option::Option* opt = options[UNKNOWN]; opt; opt = opt->next())
      	std::cout << "Unknown option: " << opt->name << "\n";
