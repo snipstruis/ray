7c45b93c4f6eaa752e667e60a8f7df148d990a66
Douwe van Gijn <kipstad@hotmail.com>
2016-12-18 17:41:35 +0100

    parking the SAHsplitter for now

--- bvh_build.h
+++ bvh_build.h
@@ -156,6 +156,30 @@ inline BVH* buildStupidBVH(Scene& s) {
     return bvh;
 }
 
+#if 0
+void AAplaneTriangle(TrianglePos const& t, int axis, float plane, glm::vec3* p0, flm::vec3* p1){
+    int points = 0;
+    glm::vec3 p[2];
+    int a1=(axis+1)%3;
+    int a2=(axis+2)%3;
+    for(int i=0;i<3;i++){
+        // if at different sides of the splitpoint...
+        glm::vec3 t0 = t.v[i];
+        glm::vec3 t1 = t.v[(i+1)%3];
+        if((t0[axis] <= plane) ^^ (t1[axis] <= plane)){
+            // intersect
+            float dx = (t0[axis]-plane)/(t0[axis]-t1[axis]);
+            p[points][axis] = plane;
+            p[points][a1] = dx*t0;
+            p[points][a2] = dx*t1;
+            points++;
+        }
+    }
+    *p0=p[0];
+    *p1=p[1];
+}
+#endif
+
 // Surface Area Heuristic splitter
 struct SAHSplitter{
     static bool GetSplit(
@@ -180,7 +204,7 @@ struct SAHSplitter{
                      
         for(int axis=0; axis<3; axis++) for(int split=1; split<8; split++){
             float trySplitPoint = bounds.low[axis] + ((float)split*(diff[axis]/8.f));
-            printf("  trying split %c=%f\n","xyz"[axis],trySplitPoint);
+            printf("  trying split %c=%f ","xyz"[axis],trySplitPoint);
             // we keep a running total of the size of the left and right bounding box
             AABB left = {{INFINITY,INFINITY,INFINITY},{-INFINITY,-INFINITY,-INFINITY}};
             AABB right= {{INFINITY,INFINITY,INFINITY},{-INFINITY,-INFINITY,-INFINITY}};
@@ -189,41 +213,75 @@ struct SAHSplitter{
             for(int t:indicies){ // for each triangle
                 TrianglePosition const& triangle = triangles[t];
                 // find out if the triangle belongs to left, right or both
-                // the bounding boxes can't overlap, but since bounding boxes
-                // are inclusive there is an edge case where the triangle lies
-                // exactly on the split plane.
                 int vertices_in_left = 0;
                 int vertices_in_right= 0;
                 for(int vertex=0; vertex<3; vertex++){
-                    glm::vec3 p = triangle.v[vertex];
-                    if(p[axis] <= trySplitPoint){
-                        vertices_in_left++;
-                        glm::vec3 p = triangle.v[vertex];
-                        left.low = glm::min(left.low,p);
-                        left.high= glm::max(left.high,p);
-                    }
-                    if(p[axis] >= trySplitPoint){
-                        vertices_in_right++;
-                        glm::vec3 p = triangle.v[vertex];
-                        right.low = glm::min(right.low,p);
-                        right.high= glm::max(right.high,p);
+                    if(triangle.v[vertex][axis] <= trySplitPoint) vertices_in_left++;
+                    if(triangle.v[vertex][axis] >= trySplitPoint) vertices_in_right++;
+                }
+                // if the triangle is completely at the left side of the
+                // splitting plane, grow the left bounding box to include the
+                // triangle
+                if(vertices_in_left==3){
+                    triangles_in_left++;
+                    for(int v=0;v<3;v++){
+                        left.low = glm::min(left.low, triangle.v[v]);
+                        left.high= glm::max(left.high,triangle.v[v]);
                     }
                 }
-                if(vertices_in_left>0)   triangles_in_left++;
-                if(vertices_in_right>0)  triangles_in_right++;
+                // same for the right side, not mutually exclusive with the 
+                // statement above because of the edge case where the
+                // triangle lies exactly on the split plane
+                if(vertices_in_right==3){
+                    triangles_in_right++;
+                    for(int v=0;v<3;v++){
+                        right.low = glm::min(right.low, triangle.v[v]);
+                        right.high= glm::max(right.high,triangle.v[v]);
+                    }
+                }
+                // if the triangle intersects the split plane, use the bounding
+                // box of the cut triangle to grow both sides
+                if(vertices_in_left!=3 && vertices_in_right!=3){
+                    triangles_in_right++;
+                    triangles_in_left++;
+                    #if 0
+                    // find intersection points
+                    glm::vec3 t0,t1;
+                    AAplaneTriangle(triangle, axis, trySplitPoint, &t0, &t1);
+                    // grow the bounding boxes including these points
+                    glm::vec3 big[4], small[3]; 
+                    if(vertices_in_left>vertices_in_right){
+                    }else{
+
+                    }
+                    #else
+                    // suboptimal but easy
+                    for(int v=0;v<3;v++){
+                        left.low   = glm::min(left.low, triangle.v[v]);
+                        left.high  = glm::max(left.high,triangle.v[v]);
+                        right.low  = glm::min(right.low, triangle.v[v]);
+                        right.high = glm::max(right.high,triangle.v[v]);
+                    }
+                    left.high[axis] = trySplitPoint;
+                    right.low[axis] = trySplitPoint;
+                    #endif
+                }
             }
             // now check if the split is the best so far
             float al = surfaceAreaAABB(left);
             float ar = surfaceAreaAABB(right);
             float area= al+ar;
             int triangle_count = indicies.size();
-            if(area<smallest_area_so_far 
-             && triangles_in_left != triangle_count
-             && triangles_in_right!= triangle_count){
+            printf("%d -> %d/%d\n",triangle_count, triangles_in_left, triangles_in_right);
+            if(area<smallest_area_so_far
+            && triangles_in_left != triangle_count
+            && triangles_in_right!= triangle_count){
                 // if it is the best so far, check if we should split at all
                 printf("  testing: al%f*tl%d+ar%f*tr%d < a%f*t%d\n",al,triangles_in_left,ar,triangles_in_right,area,triangle_count);
-                if((al*(float)triangles_in_left + ar*(float)triangles_in_right)<(area*(float)triangle_count)){
-                    printf("    best so far: area:%f triangle_L:%d triangle_R:%d total:%d\n", area, triangles_in_left, triangles_in_right, triangle_count);
+                float should_share = al*(float)triangles_in_left + ar*(float)triangles_in_right;
+                float should_stop  = area*(float)triangle_count;
+                if(should_share<should_stop){
+                    printf("    best so far: area:%f triangles_L:%d triangles_R:%d total:%d\n", area, triangles_in_left, triangles_in_right, triangle_count);
                     printf("      left:%f<%f right:%f<%f\n", left.low[axis], left.high[axis], right.low[axis], right.high[axis]);
                     // we should split, set all the output variables
                     assert(triangle_count > 1);
