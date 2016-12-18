fe04d603e33f93625ab694f1c56757335c480d4d
Nick Begg <nickbegg@gmail.com>
2016-12-18 15:23:05 +0100

    fix assert

--- bvh_build.h
+++ bvh_build.h
@@ -111,7 +111,7 @@ void subdivide(
     assert(surfaceAreaAABB(node.bounds) > 0.0f);
     assert(node.leftFirst >= 0);
     if(node.isLeaf()){
-        assert((node.count + node.leftFirst) < triangles.size());
+        assert((node.count + node.leftFirst) < bvh.indicies.size());
     }
 }
 
