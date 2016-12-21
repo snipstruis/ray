b7e7a78ea65dd0b80a6cd96f798c7bb1762b9f6e
Nick Begg <nickbegg@gmail.com>
2016-12-21 15:34:22 +0100

    start to break up render loop(s)

--- render.h
+++ render.h
@@ -35,23 +35,59 @@ Color value_to_color(float x){
     else         {x-=4; return Color(  1,1-x,  0);} // yellow-> red
 }
 
-// main render starting loop
-// assumes screenbuffer is big enough to handle the width*height pixels (per the camera)
-inline void renderFrame(Scene& s, BVH& bvh, std::vector<Color>& screenBuffer, Mode mode, float vis_scale){
-    // draw pixels
+// Do a recursive ray trace (ie the default output)
+struct StandardRenderer {
+    static Color renderPixel(Ray const& r, Scene& s, BVH& bvh, float vis_scale) {
+        Color col = trace(r, bvh, s.primitives, s.lights, Color(0,0,0));
+        return ColorClamp(col, 0.0f, 1.0f);
+    }
+};
+
+// render surface normals
+struct NormalRenderer {
+    static Color renderPixel(Ray const& r, Scene& s, BVH& bvh, float vis_scale) {
+        auto hit = findClosestIntersectionBVH(bvh, s.primitives, r);
+
+        if(hit.distance < INFINITY) {
+            // we intersected. calc normal and convert to a col
+            Triangle const& tri = s.primitives.triangles[hit.triangle];
+            TrianglePosition const& pos = s.primitives.pos[hit.triangle];
+            auto fancy = FancyIntersect(hit.distance, pos, tri, r);
+            return Color((1.f+fancy.normal.x)/2.f, (1.f+fancy.normal.y)/2.f,  (1.f+fancy.normal.z)/2.f);
+        } else {
+            // no intersection
+            return BLACK;
+        }
+    }
+}
+
+template<class PixelRenderer>
+inline void renderLoop(Scene& s, BVH& bvh, ScreenBuffer& screenBuffer, float visScale){
     int const width  = s.camera.width;
     int const height = s.camera.height;
-    switch(mode){
-    case Mode::Default:
-        #pragma omp parallel for schedule(auto) collapse(2)
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                Ray r = s.camera.makeRay(x, y);
-                int idx = (height-y-1) * width+ x;
-                auto col = trace(r, bvh, s.primitives, s.lights, Color(0,0,0));
-                screenBuffer[idx] = ColorClamp(col, 0.0f, 1.0f);
-            }
+
+    #pragma omp parallel for schedule(auto) collapse(2)
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            Ray r = s.camera.makeRay(x, y);
+            int idx = (height-y-1) * width+ x;
+            screenBuffer[idx] = PixelRenderer::renderPixel(r, s, bvh, visScale);
         }
+    }
+}
+
+// main render starting loop
+// assumes screenbuffer is big enough to handle the width*height pixels (per the camera)
+inline void renderFrame(Scene& s, BVH& bvh, ScreenBuffer& screenBuffer, Mode mode, float vis_scale){
+    int const width  = s.camera.width;
+    int const height = s.camera.height;
+
+    switch(mode) {
+    case Mode::Default:
+        renderLoop<StandardRenderer>(s, bvh, screenBuffer, vis_scale);
+        break;
+    case Mode::Normal:
+        renderLoop<NormalRenderer>(s, bvh, screenBuffer, vis_scale);
         break;
     case Mode::Microseconds:
         #pragma omp parallel for schedule(auto) collapse(2)
@@ -65,27 +101,6 @@ inline void renderFrame(Scene& s, BVH& bvh, std::vector<Color>& screenBuffer, Mo
                 auto frametime = 
                     std::chrono::duration_cast<std::chrono::duration<float,std::micro>>(end-start).count();
                 screenBuffer[idx] = value_to_color(0.01*vis_scale*frametime);
-            }
-        }
-        break;
-    case Mode::Normal:
-        #pragma omp parallel for schedule(auto) collapse(2)
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                Ray r = s.camera.makeRay(x,y);
-                int idx = (height-y-1) * width+ x;
-                auto hit = findClosestIntersectionBVH(bvh, s.primitives, r);
-                if(hit.distance < INFINITY) {
-                    Triangle const& tri = s.primitives.triangles[hit.triangle];
-                    TrianglePosition const& pos = s.primitives.pos[hit.triangle];
-                    auto fancy = FancyIntersect(hit.distance, pos, tri, r);
-                    screenBuffer[idx] = Color((1.f+fancy.normal.x)/2.f, 
-                                              (1.f+fancy.normal.y)/2.f, 
-                                              (1.f+fancy.normal.z)/2.f);
-                }
-                else {
-                    screenBuffer[idx] = BLACK;
-                }
             }
         }
         break;
