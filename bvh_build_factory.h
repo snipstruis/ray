cc052828b70249214debc295462e53c695990bb8
Nick Begg <nickbegg@gmail.com>
2016-12-21 01:16:31 +0100

    add some basic bvh stats

--- bvh_build_factory.h
+++ bvh_build_factory.h
@@ -20,12 +20,11 @@ inline BVH* buildBVH(Scene& s, BVHMethod method) {
         case __BVHMethod_MAX: assert(false); break; // shouldn't happen
     };
 
-    std::cout << "bvh node count " << bvh->nextFree << std::endl;
     std::cout << "triangle count " << s.primitives.triangles.size() << std::endl;
-    std::cout << "root AABB " << bvh->root().bounds << std::endl;
     std::cout << "BVH build time " << t.sample() << std::endl;
 
     sanityCheckBVH(*bvh, s.primitives.pos);
+    dumpBVHStats(*bvh);
 
     return bvh;
 }
--- bvh_diag.h
+++ bvh_diag.h
@@ -4,6 +4,72 @@
 #include "primitive.h"
 
 #include <iostream>
+#include <vector>
+
+struct BVHStatsPerLeaf {
+    BVHStatsPerLeaf(unsigned int _depth, unsigned int _triCount) : depth(_depth), triCount(_triCount) {}
+
+    unsigned int depth;
+    unsigned int triCount;
+};
+
+struct BVHStatsTotal {
+    BVHStatsTotal() : totalNodes(0) {}
+
+    unsigned int totalNodes;
+    std::vector<BVHStatsPerLeaf> perLeaf;
+};
+
+void dumpBVHStatsRecurse(BVH const& bvh, BVHNode const& node, int depth, BVHStatsTotal& stats) {
+    auto const& left = bvh.getNode(node.leftIndex());
+    auto const& right = bvh.getNode(node.rightIndex());
+
+    stats.totalNodes++;
+
+    if(node.isLeaf()) {
+        stats.perLeaf.emplace_back(depth, node.count);
+    } else {
+        dumpBVHStatsRecurse(bvh, left, depth + 1, stats);
+        dumpBVHStatsRecurse(bvh, right, depth + 1, stats);
+    }
+}
+
+void dumpBVHStats(BVH const& bvh){
+    BVHStatsTotal stats;
+    dumpBVHStatsRecurse(bvh, bvh.root(), 0, stats);
+
+    unsigned int maxTri = 0;
+    unsigned int minTri = std::numeric_limits<unsigned int>::max();
+    unsigned int sumTri = 0;
+
+    unsigned int maxDepth = 0;
+    unsigned int minDepth = std::numeric_limits<unsigned int>::max();
+    unsigned int sumDepth = 0;
+
+    for(auto const& p : stats.perLeaf) {
+        minTri = std::min(minTri, p.triCount);
+        maxTri = std::max(maxTri, p.triCount);
+        sumTri += p.triCount;
+
+        minDepth = std::min(minDepth, p.depth);
+        maxDepth = std::max(maxDepth, p.depth);
+        sumDepth += p.depth;
+    }
+
+    float avgDepth = sumDepth / ((float)stats.perLeaf.size());
+    float avgTri = sumTri / ((float)stats.perLeaf.size());
+
+    std::cout << "=========================================================================================\n";
+    std::cout << "BVH Stats\n";
+    std::cout << "root AABB " << bvh.root().bounds << "\n";
+    std::cout << "Total nodes " << bvh.nodes.size() << " total leaves " << stats.perLeaf.size();
+    std::cout << " Total tri indicies " << bvh.indicies.size() << "\n";
+    std::cout << "Per leaf: min tri   " << minTri << " max Tri " << maxTri << " avgTri " << avgTri << "\n";
+    std::cout << "          min depth " << minDepth << " max Depth " << maxDepth<< " avgDepth " << avgDepth<< "\n";
+
+
+    std::cout << "=========================================================================================\n";
+}
 
 // recursively check that every node fully contains its child bounds
 void sanityCheckAABBRecurse(BVH const& bvh, BVHNode const& node, TrianglePosSet const& triangles) {
